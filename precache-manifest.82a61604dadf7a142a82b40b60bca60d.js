e4dda602d89ccfacd6bcbbf85ef0857dfe30f41e
Your Name <you@example.com>
2020-10-29 10:28:40 +0200

    Updates

---
+++ precache-manifest.82a61604dadf7a142a82b40b60bca60d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b628f153b1c09134d8c439a092f90",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "47aa7d6646bc05356cb8",
+    "url": "/goit-react-hw-01-components/static/js/2.c9d2ff8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.c9d2ff8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524205fb19a77fe1a986",
+    "url": "/goit-react-hw-01-components/static/js/main.239a4dc5.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
